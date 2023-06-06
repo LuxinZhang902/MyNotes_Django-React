4ff8ce79f55e3ffefe48f2e8d8972c0434c4195c
Luxin Zhang <67583508+LuxinZhang902@users.noreply.github.com>
2023-06-06 18:10:30 -0400

    Showing only Title

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+let getTitle = (note) => {
+  const title = note.body.split('\n')[0]
+  return title
+}
+
 const ListItem = ({note}) => {
  
   return (
     <Link to={'/note/' + note.id}>
       <div className='notes-list-item'>
-        <h3>{note.body}</h3>
+        <h3>{getTitle(note)}</h3>
       </div>
       
     </Link>
